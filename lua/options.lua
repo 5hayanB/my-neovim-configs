f3373bd3ab5c2fb6b97b8558d43bf0d62524599d
CyberGai <cyb3rgai@gmail.com>
2022-07-29 03:28:54 +0500

    added more options

--- lua/options.lua
+++ lua/options.lua
@@ -1,6 +1,13 @@
 local options = {
     termguicolors = true,
-    shiftwidth = 4
+    shiftwidth = 4,
+    wrap = false,
+    tabstop = 4,
+    number = true,
+    cursorline = true,
+    smartindent = true,
+    fileencoding = 'utf-8',
+    mouse = 'a'
 }
 
 for k, v in pairs(options) do
